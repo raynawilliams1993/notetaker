dccce55e24af17a5ec2e942a650460ebebc6ead1
rayna williams <Raynawilliams123@gmail.com>
2020-01-23 21:52:02 -0700

    added a read me and deployed image of the app

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# notetaker
+----
+ABOUT:
+This is an app that is using the backend to allow the user to take notes and save them.
+The application frontend has already been created, using Api calls and routes for the backend to connect the two.
+
+----
+USER STORY:
+AS A user, I want to be able to write and save notes
+
+I WANT to be able to delete notes I've written before
+
+SO THAT I can organize my thoughts and keep track of tasks I need to complete.
+
+
+----
+TECHNOLOGIES USED:
+server.js, node, LOCAL PORT 3000, styles.css, db.json, npm, notes api, html, github
+
+----
+
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var fs = require("fs");
 
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,27 +23,39 @@ app.get("/notes", function (req, res) {
 
 //create 3 api routes one gets the data from db.json, one gets and creates data and posts it to db.json, one that deletes based off of id.
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    // Need to determine what req.body will look like
-    app.post("/api/notes", function(req, res) {
+    app.post("/api/notes", function (req, res) {
         note = JSON.stringify(req.body);
         console.log(note);
-        console.log(path.join(__dirname , "../db/db.json"))
-        fs.appendFileSync(path.join(__dirname , "../db/db.json"), note, err => {
+        console.log(path.join(__dirname, "../db/db.json"))
+        fs.appendFileSync(path.join(__dirname, "../db/db.json"), note, err => {
             if (err) throw err;
             console.log("Wrote to db.json")
         });
         res.send(true);
         // New comment line
     });
-    app.get("/api/notes", function(req, res) {
+    app.get("/api/notes", function (req, res) {
         console.log(notes)
         res.json(notes);
     });
 }
 
 //figure out a way to keep track of id's in db.json and create new ids are unique (hotresturaunt)
+// $("#add-btn").on("click", function(event) {
+	//       event.preventDefault();//keeps the page from refreshing.
+	//         name: $("#name").val().trim(),
+	//         role: $("#role").val().trim(),
+	//         age: $("#age").val().trim(),
+	//         forcePoints: $("#force-points").val().trim()
+	//       };
+	//       // Question: What does this code do??
+	//       $.post("/api/characters", newCharacter)
+	//         .then(function(data) {
+	//           console.log("add.html", data);
+	//           alert("Adding character...");	
+	//         });
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
