ab5bc1dbbda28a1e8cce0d060c66f3af5e38109d
rayna williams <Raynawilliams123@gmail.com>
2020-01-23 21:24:06 -0700

    got api calls working

--- server.js
+++ server.js
@@ -22,6 +22,25 @@ app.get("/notes", function (req, res) {
 
 //create 3 api routes one gets the data from db.json, one gets and creates data and posts it to db.json, one that deletes based off of id.
 
+module.exports = function(app) {
+
+    // Need to determine what req.body will look like
+    app.post("/api/notes", function(req, res) {
+        note = JSON.stringify(req.body);
+        console.log(note);
+        console.log(path.join(__dirname , "../db/db.json"))
+        fs.appendFileSync(path.join(__dirname , "../db/db.json"), note, err => {
+            if (err) throw err;
+            console.log("Wrote to db.json")
+        });
+        res.send(true);
+        // New comment line
+    });
+    app.get("/api/notes", function(req, res) {
+        console.log(notes)
+        res.json(notes);
+    });
+}
 
 //figure out a way to keep track of id's in db.json and create new ids are unique (hotresturaunt)
 
